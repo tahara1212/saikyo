a7a61b452742100b44376c30bb9e9950b884c900
田原　隼併 <01047934@CF3717.local>
2024-01-26 10:50:42 +0900

    app router: 論理グループのテスト

--- app/(Learn)/202401/page.tsx
+++ app/(Learn)/202401/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>app main</h1>
-      <Link href="202401" className="text-blue-200 underline">2024/01</Link>
+      <h1>202401</h1>
+      <Link href="/" className="text-blue-200 underline">Home</Link>
     </main>
   );
 }
