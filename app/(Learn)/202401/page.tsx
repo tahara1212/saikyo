a00cb68260fef52f468120a8ba554c46a09e8c19
田原　隼併 <t.pe1shun@gmail.com>
2024-01-26 17:15:44 +0900

    react-syntax-highlighter の導入

--- app/(Learn)/202401/page.tsx
+++ app/(Learn)/202401/page.tsx
@@ -1,10 +1,31 @@
-import Image from "next/image";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import Link from "next/link";
 
 export default function Home() {
+  const codeString = `
+    // MyReadonly<T>を実装してください
+    // MyReadonlyは、Tのプロパティをすべて読み取り専用にする型です
+    type MyReadonly<T> = {
+      readonly [K in keyof T]: T[K]
+    }
+    
+    interface Todo {
+      title: string;
+      description: string;
+    }
+    
+    const todo: MyReadonly<Todo> = {
+      title: "Hey",
+      description: "foobar",
+    };
+  `
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>202401</h1>
+      <SyntaxHighlighter language="typescript" style={dark}>
+        {codeString.trim()}
+      </SyntaxHighlighter>
       <Link href="/" className="text-blue-200 underline">Home</Link>
     </main>
   );
