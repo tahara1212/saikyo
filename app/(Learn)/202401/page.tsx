9aa8101b2ef31d60cfb9fbb3fd15e29ca129df02
田原　隼併 <t.pe1shun@gmail.com>
2024-01-29 10:03:14 +0900

    20240129 - type-challenges

--- app/(Learn)/202401/page.tsx
+++ app/(Learn)/202401/page.tsx
@@ -1,5 +1,5 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import Link from "next/link";
 
 export default function Home() {
@@ -20,11 +20,32 @@ export default function Home() {
       description: "foobar",
     };
   `
+  const codeString02 = `
+  // 配列の長さを返すLength<T>を実装してください。
+  type Length<T　extends number[] | string[]> = T['length']
+
+  type tesla = ["tesla", "model 3", "model X", "model Y"];
+  type spaceX = [
+    "FALCON 9",
+    "FALCON HEAVY",
+    "DRAGON",
+    "STARSHIP",
+    "HUMAN SPACEFLIGHT"
+  ];
+
+  type teslaLength = Length<tesla>; // expected 4
+  type spaceXLength = Length<spaceX>; // expected 5
+`
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>組み込みの型ユーティリティReadonly<T>を使用せず、T のすべてのプロパティを読み取り専用にする型を実装します。実装された型のプロパティは再割り当てできません。</h1>
-      <SyntaxHighlighter language="typescript" style={dark}>
+      <h1>{`組み込みの型ユーティリティReadonly<T>を使用せず、T のすべてのプロパティを読み取り専用にする型を実装します。実装された型のプロパティは再割り当てできません。`}</h1>
+      <SyntaxHighlighter language="typescript" style={tomorrow}>
         {codeString.trim()}
+      </SyntaxHighlighter>
+      <br/>
+      <h1>{`タプルTを受け取り、そのタプルの長さを返す型Length<T>を実装します。`}</h1>
+      <SyntaxHighlighter language="typescript" style={tomorrow}>
+        {codeString02.trim()}
       </SyntaxHighlighter>
       <Link href="/" className="text-blue-200 underline">Home</Link>
     </main>
