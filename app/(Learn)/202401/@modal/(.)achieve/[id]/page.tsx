dab562d8e9bf663e881896b2e01eb545d3994334
sh-tahara <t.pe1shun@gmail.com>
2024-04-05 18:27:46 +0900

    modal画面の基礎となるスタイルを定義

--- app/(Learn)/202401/@modal/(.)achieve/[id]/page.tsx
+++ app/(Learn)/202401/@modal/(.)achieve/[id]/page.tsx
@@ -5,8 +5,17 @@ import matter from 'gray-matter';
 import { Modal } from './modal';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { ParsedUrlQuery } from 'querystring';
 
-export default function ModalPage({ params }) {
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+interface ModalPageProps {
+  params: Params;
+}
+
+export default function ModalPage({ params }: ModalPageProps) {
   const { id } = params;
   const filePath = path.join(process.cwd(), 'app/_docs/202401', `${id}.md`);
   const fileContents = fs.readFileSync(filePath, 'utf8');
@@ -14,7 +23,7 @@ export default function ModalPage({ params }) {
   return (
     <Modal>
       {/* react-markdown を使用してマークダウンの内容をレンダリングします */}
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} className='markdown'>
         {content}
       </ReactMarkdown>
     </Modal>
